42dbbc279a8bb8417448a4190dc5f5f56c3e32b4
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-10-28 10:44:31 -0300

    deploy do projeto

---
+++ portfolio_react/src/components/AboutContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutContainer = () => {
+  return (
+    <section className='about-container'>
+    <h2>Sobre</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sapiente est commodi atque necessitatibus vero! Praesentium, error! Doloribus non doloremque ipsam quia nam minus modi illo, nulla aliquam et! Ratione!
+    </p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi fuga ducimus ut nisi distinctio, reiciendis delectus rem esse cupiditate repellat veritatis minus laboriosam ullam eaque. Doloremque voluptatibus nam magni hic.</p>
+   </section>
+  )
+}
+
+export default AboutContainer
---
+++ portfolio_react/src/components/ProjectsContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProjectsContainer() {
+  return (
+   <section className='projects-container'>
+    <h2>Projetos</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati officiis voluptatibus ducimus illum enim reprehenderit iste non, eligendi error accusamus repellat! Soluta quam ab quaerat, nihil impedit doloribus possimus cupiditate?
+    </p>
+    <a href="#" className='btn'></a>
+   </section>
+  )
+}
+
+export default ProjectsContainer
